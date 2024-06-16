e0334c4d63d41d5ac478cfe8db1c514c3113b6d0
Kuba0517 <jakub.ratusz@gmail.com>
2024-06-16 21:24:25 +0200

    Everything working

---
+++ APBD11/Migrations/20240616191110_Add salt to user.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JWT.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addsalttouser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "salt",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "salt",
+                table: "Users");
+        }
+    }
+}
